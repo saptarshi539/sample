ec86a1964849c91ed13e80e8a162e52451befb18
saptarshi539 <saptarshi539@gmail.com>
2017-09-13 03:37:59 -0400

    Practice test

---
+++ Sample/src/practiceProblems/MaxNumberofMoves.java
@@ -0,0 +1,17 @@
+package practiceProblems;
+
+public class MaxNumberofMoves {
+
+
+
+
+public static void main(String args[])
+{
+	int arr[] = { 5, 6, 4, 2, 8, 3, 1 };
+    int k = 3, i = 3;
+    int n = arr.length ;
+    System.out.println( "Maximum points :  maximumPoints(arr, n, k - 1, i)");
+    //return 0;
+}
+
+}
